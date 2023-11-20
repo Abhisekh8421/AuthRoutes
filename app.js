103dc3c9c1b680cb55209ac02e550b3994f88f26
Suru Abhisekh <abhisekhsuru@gmail.com>
2023-11-20 21:11:46 +0530

    .env changes

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
